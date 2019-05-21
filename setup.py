16885662d6dfcb220e70ad66e5b011e3366849c1
Sergio Oller <sergioller@gmail.com>
2019-05-21 09:14:13 +0200

    Release 1.5.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as file:
     long_description = file.read()
 
 setup(name='parmap',
-      version='1.5.1.9000',
+      version='1.5.2',
       description=('map and starmap implementations passing additional '
                    'arguments and parallelizing if possible'),
       long_description=long_description,
