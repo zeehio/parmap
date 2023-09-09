455fd997a0186b177946e86e08339f59cb7c4e5a
Sergio Oller <sergioller@gmail.com>
2023-09-09 18:01:16 +0200

    black

--- parmap/__init__.py
+++ parmap/__init__.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
 from .parmap import map, map_async, starmap, starmap_async
 
-__all__ = ['map', 'starmap', 'map_async', 'starmap_async']
-
+__all__ = ["map", "starmap", "map_async", "starmap_async"]
